b63c44a28ff15d26464b019b086a0041e50ddc4b
Carl Cronsioe <carl.cronsioe@kambi.com>
2024-04-30 09:06:51 +0200

    Filter

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import { productsQueryOptions } from "../products";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import ProductCard from "../components/productCard";
+import { useState } from "react";
 
 export const Route = createFileRoute("/")({
   loader: ({ context: { queryClient } }) =>
@@ -10,14 +11,76 @@ export const Route = createFileRoute("/")({
 });
 
 function HomePage() {
-  const postsQuery = useSuspenseQuery(productsQueryOptions);
-  const posts = postsQuery.data;
+  const productsQuery = useSuspenseQuery(productsQueryOptions);
+  const [titleFilter, setTitleFilter] = useState("");
+  const [categoryFilter, setCategoryFilter] = useState("");
+  const [priceFilter, setPriceFilter] = useState("");
+
+  const products = productsQuery.data;
+  const availableCategories = [
+    ...new Set(products.map((product) => product.category)),
+  ];
+
+  const filteredProducts = products.filter((product) => {
+    let titleMatch = true;
+    let categoryMatch = true;
+    let priceMatch = true;
+
+    if (titleFilter) {
+      titleMatch = product.title
+        .toLocaleLowerCase()
+        .includes(titleFilter.toLowerCase());
+    }
+    if (categoryFilter) {
+      categoryMatch = product.category === categoryFilter;
+    }
+
+    if (priceFilter) {
+      priceMatch = product.price <= parseFloat(priceFilter);
+    }
+
+    return titleMatch && categoryMatch && priceMatch;
+  });
 
   return (
-    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-4 md:px-6 py-12">
-      {posts?.map((product) => {
-        return <ProductCard key={product.id} product={product}></ProductCard>;
-      })}
-    </section>
+    <div>
+      <div>
+        Filter
+        <input
+          type="text"
+          value={titleFilter}
+          onChange={(e) => setTitleFilter(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="categoryFilter">Category</label>
+        <select
+          name="categoryFilter"
+          id="categoryFilter"
+          onChange={(e) => setCategoryFilter(e.target.value)}
+        >
+          <option value=""></option>
+
+          {availableCategories.map((c) => (
+            <option value={c}>{c}</option>
+          ))}
+        </select>
+      </div>
+
+      <div>
+        Price filter
+        <input
+          type="number"
+          value={priceFilter}
+          onChange={(e) => setPriceFilter(e.target.value)}
+        />
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-4 md:px-6 py-12">
+        {filteredProducts?.map((product) => {
+          return <ProductCard key={product.id} product={product}></ProductCard>;
+        })}
+      </div>
+    </div>
   );
 }
