8f6a3545de3e26cc0bfe4caf270d397b082d4bd4
Carl Cronsioe <carl.cronsioe@kambi.com>
2024-05-01 20:39:03 +0200

    debugging vercel deployment problem

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,7 +2,6 @@ import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Header } from "../components/header";
 
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient;
@@ -13,7 +12,6 @@ export const Route = createRootRouteWithContext<{
 function RootComponent() {
   return (
     <>
-      <Header />
       <Outlet />
       <ReactQueryDevtools buttonPosition="top-right" />
       <TanStackRouterDevtools position="bottom-right" />
