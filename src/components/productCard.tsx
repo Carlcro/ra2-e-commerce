e6584f752139e1ffcba2c249689e95bdfbfb870e
Carl Cronsioe <carl.cronsioe@kambi.com>
2024-05-01 07:12:40 +0200

    hmm

--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -15,45 +15,47 @@ export default function ProductCard({
   const { addItem } = useCart();
 
   return (
-    <div className="bg-white rounded-lg shadow-md group">
-      <Link className="block relative" to={`/products/${id}`}>
-        <img
-          alt="Product Image"
-          className="w-full h-64 object-scale-down group-hover:opacity-80 transition-opacity"
-          height={400}
-          src={`${image}`}
-          width={600}
-        />
-      </Link>
-      <div className="p-4 h-full justify-evenly">
-        <div>
-          <div className="flex items-center justify-between mb-2">
-            <span className="text-sm font-medium text-gray-500 ">
-              {category}
-            </span>
-            <div className="flex items-center gap-2 text-gray-500 ">
-              <span className="text-sm font-medium">{rating.rate}</span>
-              <span className="text-sm font-medium">({rating.count})</span>
+    <div className="flex flex-col justify-between bg-white rounded-lg shadow-md group p-4">
+      <div>
+        <Link className="block relative" to={`/products/${id}`}>
+          <img
+            alt="Product Image"
+            className="w-full h-64 object-scale-down group-hover:opacity-80 transition-opacity"
+            height={400}
+            src={`${image}`}
+            width={600}
+          />
+        </Link>
+        <div className="justify-between mt-2">
+          <div>
+            <div className="flex items-center justify-between mb-2">
+              <span className="text-sm font-medium text-gray-500 ">
+                {category}
+              </span>
+              <div className="flex items-center gap-2 text-gray-500 ">
+                <span className="text-sm font-medium">{rating.rate}</span>
+                <span className="text-sm font-medium">({rating.count})</span>
+              </div>
             </div>
+            <h3 className="text-lg font-semibold mb-2">
+              <Link to={`/products/${id}`}>{title}</Link>
+            </h3>
+            <p
+              className={`text-gray-500 mb-4 ${showFullDescription ? "" : "line-clamp-4"}`}
+            >
+              {description}
+            </p>
           </div>
-          <h3 className="text-lg font-semibold mb-2">
-            <Link to={`/products/${id}`}>{title}</Link>
-          </h3>
-          <p
-            className={`text-gray-500 mb-4 ${showFullDescription ? "" : "line-clamp-4"}`}
-          >
-            {description}
-          </p>
         </div>
-        <div className="flex items-center justify-between">
-          <span className="font-semibold">{`${price} .-`}</span>
-          <button
-            onClick={() => addItem(product)}
-            className="bg-black text-white px-3 py-2 rounded-md"
-          >
-            Add to Cart
-          </button>
-        </div>
+      </div>
+      <div className="flex items-center justify-between">
+        <span className="font-semibold">{`${price} .-`}</span>
+        <button
+          onClick={() => addItem(product)}
+          className="bg-black text-white px-3 py-2 rounded-md"
+        >
+          Add to Cart
+        </button>
       </div>
     </div>
   );
