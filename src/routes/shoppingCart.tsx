f6b8e0a4d641bf3df77de98c6a8742eef2391802
Carl Cronsioe <carl.cronsioe@kambi.com>
2024-05-01 21:21:33 +0200

    Added text and removed go to checkout button if shopping cart is empty

--- src/routes/shoppingCart.tsx
+++ src/routes/shoppingCart.tsx
@@ -13,16 +13,22 @@ function ShoppingCart() {
     <div className="flex flex-col items-center  max-w-[800px] mx-auto">
       <h2 className="text-2xl">Shopping Cart</h2>
       <div className="flex flex-col gap-4 mt-5 w-full">
-        {cartItems.map((item) => (
-          <ShoppingCartCard item={item} />
-        ))}
+        {cartItems.length === 0 ? (
+          <span className="text-center">Shopping cart is empty</span>
+        ) : (
+          <>
+            {cartItems.map((item) => (
+              <ShoppingCartCard key={item.product.id} item={item} />
+            ))}
+            <Link
+              className="w-full bg-black text-white py-2 flex items-center justify-center mt-5"
+              to="/checkout"
+            >
+              Go to Checkout
+            </Link>
+          </>
+        )}
       </div>
-      <Link
-        className="w-full bg-black text-white py-2 flex items-center justify-center mt-5"
-        to="/checkout"
-      >
-        Go to Checkout
-      </Link>
     </div>
   );
 }
